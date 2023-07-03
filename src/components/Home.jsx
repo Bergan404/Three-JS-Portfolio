03bc2818d630a2e835e88851452cff25f6cd04ae
Bergan Oudshoorn <bergan404@gmail.com>
2023-07-03 14:59:46 -0400

    more work on the hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,31 +10,27 @@ const Home = () => {
     return (
         <section className={`relative w-full h-screen mx-auto`}>
             <div
-                className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+                className={`xl: flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
             >
                 <div
                     className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
                 >
                     <div className='flex flex-col justify-center items-center mt-5'>
-                        <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-                        <div className='w-1 sm:h-80 h-40 violet-gradient' />
+                        <div className='w-5 h-5 rounded-full bg-[#8d6eff]' />
+                        <div className='w-1 sm:h-80 h-40 violet-full' />
+                        <div className='w-5 h-5 rounded-full bg-[#8d6eff]' />
                     </div>
 
                     <div>
                         <h1 className={`${styles.heroHeadText} text-white`}>
-                            Hi, I'm <span className='text-[#915EFF]'>Bergan</span>
+                            Hi, I'm <span className='text-[#8d6eff]'>Bergan</span>
                         </h1>
                         <p className={`${styles.heroSubText} mt-2 text-white-100`}>
                             I'm a Software Developer creating<br className='sm:block hidden' />
                             user interfaces and web applications
                         </p>
                     </div>
-                    <motion.div
-                        variants={slideIn("right", "tween", 0.2, 1)}
-                        className='xl:flex-1 xl: md:h-[600px] h-[350px]'
-                    >
                         <EarthCanvas />
-                    </motion.div>
                 </div>
 
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Bergan &nbsp;
-            <span className='sm:block hidden'> | Software Devel</span>
+            <span className='sm:block hidden'> | Software Developer</span>
           </p>
         </Link>
 
