22100646847d091874ec2f66c22d432d6ebd566f
Bergan Oudshoorn <bergan404@gmail.com>
2023-07-04 17:01:51 -0400

    work experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,6 +21,7 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
+import harrismedia from "./company/harrismedia.png"
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
@@ -52,6 +53,7 @@ export {
   tailwind,
   typescript,
   threejs,
+  harrismedia,
   meta,
   shopify,
   starbucks,
--- src/constants/index.js
+++ src/constants/index.js
@@ -16,6 +16,7 @@ import {
   figma,
   docker,
   meta,
+  harrismedia,
   starbucks,
   tesla,
   shopify,
@@ -116,57 +117,21 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
+    title: "Full Stack Developer",
+    company_name: "Harris Media LLC",
+    icon: harrismedia,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "March 2022 - Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Develop entire websites for Wordpress and Nationbuilder creating custom themes using HTML, CSS, SCSS, Javascript, jQuery, PHP, and Liquid.",
+      "Maintain existing websites.",
+      "Create new components for landing pages, popups, petitions, etc..",
+      "Balance and maintain multiple projects at a time.",
+      "Make sure sites are responsive and look great across all devices.",
+      "Various Wordpress and Nationbuilder tasks. (form tweaks, plugin maintenance, etc...)",
+      "DNS tasks including domain and SSL setup."
     ],
-  },
-  {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
+  }
 ];
 
 const testimonials = [
